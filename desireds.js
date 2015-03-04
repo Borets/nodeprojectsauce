a144846395b3045628a6bc2efeb8005403b82d45
Yaroslav Borets <yaroslav@Yaroslavs-MacBook-Air.local>
2015-03-03 17:32:49 -0800

    first commit

---
+++ desireds.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+module.exports = {
+    chrome: {browserName: 'chrome', platform: 'Mac OS X 10.9' },
+    firefox: {browserName: 'iphone', platform: 'OS X 10.10', version: '8.0', deviceName: 'iPhone Simulator' },
+    explorer: {browserName: 'internet explorer'}
+
+
+};
+
+/*
+module.exports = {
+    firefox: {browserName: 'iphone', platform: 'OS X 10.10', version: '8.0', deviceName: 'iPhone Simulator', device-orientation: 'portrait' },
+    chrome: {browserName: 'firefox', platform: 'Mac OS X 10.9'},
+    explorer: {browserName: 'internet explorer'}
+
+
+};
+*/
