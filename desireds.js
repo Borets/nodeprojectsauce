2b419dadfd8496487dc84cd0b8e7d9a0f0185bd1
Yaroslav Borets <yaroslav@Yaroslavs-MacBook-Air.local>
2015-03-03 17:57:09 -0800

    script

--- desireds.js
+++ desireds.js
@@ -3,18 +3,8 @@
 
 module.exports = {
     chrome: {browserName: 'chrome', platform: 'Mac OS X 10.9' },
-    firefox: {browserName: 'iphone', platform: 'OS X 10.10', version: '8.0', deviceName: 'iPhone Simulator' },
+    firefox: {browserName: 'firefox'},
     explorer: {browserName: 'internet explorer'}
 
 
 };
-
-/*
-module.exports = {
-    firefox: {browserName: 'iphone', platform: 'OS X 10.10', version: '8.0', deviceName: 'iPhone Simulator', device-orientation: 'portrait' },
-    chrome: {browserName: 'firefox', platform: 'Mac OS X 10.9'},
-    explorer: {browserName: 'internet explorer'}
-
-
-};
-*/
